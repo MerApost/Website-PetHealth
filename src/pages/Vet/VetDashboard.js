1bafc180ce7e387c1c26ee250eabec8cf2f9f8b5
merapost <sdi2200008@uoa.gr>
2026-02-27 14:01:14 +0200

    allagew dashboard

--- src/pages/Vet/VetDashboard.js
+++ src/pages/Vet/VetDashboard.js
@@ -4,6 +4,7 @@ import PetsIcon from "@mui/icons-material/Pets";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import HistoryIcon from "@mui/icons-material/History";
 import DescriptionIcon from "@mui/icons-material/Description";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
 import { useNavigate, useParams } from "react-router-dom";
 
 const drawerWidth = 270;
@@ -51,30 +52,6 @@ export default function VetDashboard({ active }) {
 
         <ListItem disablePadding>
           <ListItemButton
-            onClick={() => go("/arrange_appointment")}
-            sx={{ backgroundColor: active === "appointments" ? "#D7D3CB" : "transparent" }}
-          >
-            <ListItemIcon>
-              <SearchIcon sx={{ color: "black" }} />
-            </ListItemIcon>
-            <ListItemText primary="Διαχείριση Ραντεβού" />
-          </ListItemButton>
-        </ListItem>
-
-        <ListItem disablePadding>
-          <ListItemButton
-            onClick={() => go("/schedule")}
-            sx={{ backgroundColor: active === "schedule" ? "#D7D3CB" : "transparent" }}
-          >
-            <ListItemIcon>
-              <CalendarMonthIcon sx={{ color: "black" }} />
-            </ListItemIcon>
-            <ListItemText primary="Ορισμός Διαθεσιμότητας" />
-          </ListItemButton>
-        </ListItem>
-
-        <ListItem disablePadding>
-          <ListItemButton
             onClick={() => go("/pet-register")}
             sx={{ backgroundColor: active === "pet-register" ? "#D7D3CB" : "transparent" }}
           >
@@ -82,6 +59,18 @@ export default function VetDashboard({ active }) {
               <PetsIcon sx={{ color: "black" }} />
             </ListItemIcon>
             <ListItemText primary="Καταγραφή Ταυτότητας Κατοικιδίου" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton
+            onClick={() => go("/arrange_appointment")}
+            sx={{ backgroundColor: active === "appointments" ? "#D7D3CB" : "transparent" }}
+          >
+            <ListItemIcon>
+              <SearchIcon sx={{ color: "black" }} />
+            </ListItemIcon>
+            <ListItemText primary="Διαχείριση Ραντεβού" />
           </ListItemButton>
         </ListItem>
 
@@ -99,11 +88,23 @@ export default function VetDashboard({ active }) {
 
         <ListItem disablePadding>
           <ListItemButton
+            onClick={() => go("/schedule")}
+            sx={{ backgroundColor: active === "schedule" ? "#D7D3CB" : "transparent" }}
+          >
+            <ListItemIcon>
+              <CalendarMonthIcon sx={{ color: "black" }} />
+            </ListItemIcon>
+            <ListItemText primary="Ορισμός Διαθεσιμότητας" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton
             onClick={() => go("/reviews")}
             sx={{ backgroundColor: active === "reviews" ? "#D7D3CB" : "transparent" }}
           >
             <ListItemIcon>
-              <HistoryIcon sx={{ color: "black" }} />
+              <StarBorderIcon sx={{ color: "black" }} />
             </ListItemIcon>
             <ListItemText primary="Προβολή Αξιολογήσεων" />
           </ListItemButton>
